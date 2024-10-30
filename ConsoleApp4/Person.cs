2423503b6f2b3ddb024295ba9c0b55c637208d71
poravoz <116353344+poravoz@users.noreply.github.com>
2024-10-30 14:08:24 +0200

    30.10.2024

---
+++ ConsoleApp4/Person.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Reflection;
+
+namespace ConsoleApp4
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+		private string LastName { get; set; }
+		protected int Age { get; set; }
+		internal double Height { get; set; }
+		private bool isMarried;
+
+		public Person(string firstName, string lastName, int age, double height, bool isMarried)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+			Height = height;
+			this.isMarried = isMarried;
+		}
+
+		public string GetFullName()
+		{
+			return $"{FirstName} {LastName}";
+		}
+
+		public void Salutation()
+		{
+			Console.WriteLine($"Привіт, мене звати {GetFullName()}!");
+		}
+
+		public string CheckMarriedStatus()
+		{
+			return isMarried ? "Одружений/Заміжня" : "Неодружений/Неодружена";
+		}
+	}
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,63 @@
+﻿using ConsoleApp4;
+using System.Reflection;
+
+public class Program
+{
+	public static void Main()
+	{
+		// Type
+		Console.WriteLine("Type");
+		var assembly = Assembly.GetExecutingAssembly();
+		var types = assembly.GetTypes();
+		foreach (var type in types)
+		{
+			var typeInfo = type.GetTypeInfo();
+			Console.WriteLine("Type " + type.FullName + " has " + typeInfo.DeclaredProperties.Count().ToString() + " properties");
+		}
+		Console.WriteLine();
+
+		// TypeInfo
+		Console.WriteLine("TypeInfo");
+		TypeInfo personInfo = typeof(Person).GetTypeInfo();
+		IEnumerable<PropertyInfo> declaredProperties = personInfo.DeclaredProperties;
+		Console.WriteLine("Властивостi класу:");
+		foreach (PropertyInfo property in declaredProperties)
+		{
+			Console.WriteLine($"{property.Name} (тип: {property.PropertyType})");
+		}
+
+		IEnumerable<MethodInfo> declaredMethods = personInfo.DeclaredMethods;
+		Console.WriteLine("\nМетоди класу:");
+		foreach (MethodInfo method in declaredMethods)
+		{
+			Console.WriteLine($"{method.Name}");
+		}
+		Console.WriteLine();
+
+		// Member Info
+		Console.WriteLine("MemberInfo");
+		Console.WriteLine("Single Type is {0}\n", personInfo);
+		MemberInfo[] mbrInfoArray = personInfo.GetMembers();
+		foreach (MemberInfo mbrInfo in mbrInfoArray)
+		{
+			Console.WriteLine("{0}			{1}", mbrInfo.MemberType, mbrInfo.Name);
+		}
+		Console.WriteLine();
+
+		// FieldInfo
+		Console.WriteLine("FieldInfo");
+		FieldInfo[] fields = personInfo.DeclaredFields.ToArray();
+		foreach (FieldInfo field in fields)
+		{
+			Console.WriteLine($"{field.Name} (тип: {field.FieldType}) - Доступ: {field.Attributes}");
+		}
+		Console.WriteLine();
+
+		//Reflection
+		Console.WriteLine("Reflection");
+		Person person = new Person("Микола", "Рудь", 19, 175, false);
+		MethodInfo checkMarriedStatusMethod = personInfo.GetMethod("CheckMarriedStatus");
+		string marriedStatus = (string)checkMarriedStatusMethod.Invoke(person, null);
+		Console.WriteLine($"Статус: {marriedStatus}");
+	}
+}
